e9b23c601fc155441c7139b362795673ac23f4f2
LuismiJava <luismilabrada@gmail.com>
2021-04-15 18:51:34 +0200

    actualiza

---
+++ resources/views/datatable.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.plantilla')
+
+
+@section('title', 'Tabla de Cursos')
+    
+@section('css')
+    
+@endsection
+
+@section('content')
+
+<div class="card">
+
+<div class="card-body">
+
+<h1>Estos son los cursos que tenemos disponibles</h1>
+
+<table class="table table-striped" id="cursos">
+
+<thead>
+
+    <tr>
+
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Descripcion</th>
+        <th>Categoría</th>
+        <th>Creado en</th>
+
+    </tr>
+
+</thead>
+
+
+@foreach ($cursos as $curso)
+
+
+<tr>
+
+    <td>{{$curso->id}}</td>
+    <td>{{$curso->nombre}}</td>
+    <td>{{$curso->descripcion}}</td>
+    <td>{{$curso->categoria}}</td>
+    <td>{{$curso->created_at->diffForHumans()}}</td>
+
+
+
+</tr>
+
+
+    
+@endforeach
+
+</table>
+</div>
+</div>
+
+
+@endsection
+
+
+@section('js')
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+<script>
+ $('#cursos').DataTable();
+
+</script>
+
+
+@endsection
