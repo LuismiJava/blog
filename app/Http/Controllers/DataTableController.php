238b34b0eaf47718cb083c5f6acb7c3f3430fff8
LuismiJava <luismilabrada@gmail.com>
2021-04-15 18:53:24 +0200

    actualizacion

--- app/Http/Controllers/DataTableController.php
+++ app/Http/Controllers/DataTableController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class DataTableController extends Controller
 {
     public function index()
     {
+        $cursos = Curso::all();
         return view('datatable');
     }
 }
